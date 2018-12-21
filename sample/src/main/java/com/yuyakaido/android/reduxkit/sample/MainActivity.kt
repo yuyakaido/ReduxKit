93c8b21e3d3a73f403f691ab6766b2835b8f9a7d
Yuya Kaido <yuyakaido@gmail.com>
2018-12-22 04:51:30 +0900

    Start server in android device

--- sample/src/main/java/com/yuyakaido/android/reduxkit/sample/MainActivity.kt
+++ sample/src/main/java/com/yuyakaido/android/reduxkit/sample/MainActivity.kt
@@ -2,12 +2,21 @@ package com.yuyakaido.android.reduxkit.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.yuyakaido.android.reduxkit.server.Server
 
 class MainActivity : AppCompatActivity() {
+
+    private val server = Server()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        server.start()
+    }
+
+    override fun onDestroy() {
+        server.stop()
+        super.onDestroy()
     }
 
 }
--- server/src/main/java/com/yuyakaido/android/reduxkit/server/Server.java
+++ server/src/main/java/com/yuyakaido/android/reduxkit/server/Server.java
@@ -1,3 +1,16 @@
 package com.yuyakaido.android.reduxkit.server;
 
-public class Server {}
+import fi.iki.elonen.NanoHTTPD;
+
+public class Server extends NanoHTTPD {
+
+    public Server() {
+        super(8080);
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        return newFixedLengthResponse("Hello!");
+    }
+
+}
